d0e0d74d0ad3a5ba5f33dc932b04041f93e4574e
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2018-11-30 13:21:13 +1100

    Section 3 Lecture 20 - return new isntance of type

--- deck.go
+++ deck.go
@@ -5,6 +5,21 @@ import "fmt"
 // create a new type of deck which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
